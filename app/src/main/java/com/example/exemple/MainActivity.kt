7264ca2388bd7b024d666388d2fe8630db8efc87
Nicolas Quatela <nicolas.quatela@etu?univ-cotedazur.fr>
2025-01-06 11:51:17 +0100

    etape 2 bis

--- app/src/main/java/com/example/exemple/MainActivity.kt
+++ app/src/main/java/com/example/exemple/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.exemple
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,10 +17,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button3)
+        rollButton.isEnabled = false
+
 
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        val input: EditText = findViewById(R.id.input)
+
+
+        input.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                rollButton.isEnabled = isInputValid(s)
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+
+    }
+
+    private fun isInputValid(input: CharSequence?): Boolean {
+        if (input.isNullOrEmpty()) return false
+        val number = input.toString().toIntOrNull()
+        return number != null && number in 2..12
     }
 
     private fun rollDice() {
@@ -25,14 +49,19 @@ class MainActivity : AppCompatActivity() {
         val dice2 = Dice(6)
         var diceRoll = dice.roll()
         var diceRoll2 = dice2.roll()
+        val sum = diceRoll + diceRoll2
         val resultTextView1: TextView = findViewById(R.id.de)
         val resultTextView2: TextView = findViewById(R.id.de2)
         resultTextView1.text = diceRoll.toString()
         resultTextView2.text = diceRoll2.toString()
 
-        if (diceRoll == diceRoll2) {
-            val winTextView: TextView = findViewById(R.id.win)
-            winTextView.text = "ET CEST GAGNÉ!!!!!!!!!!!!!!!!!"
+
+        val input: EditText = findViewById(R.id.input)
+        val userInput = input.text.toString().toIntOrNull() // Transforme l'input en entier
+
+        val winTextView: TextView = findViewById(R.id.win)
+        if (userInput != null && sum == userInput) {
+            winTextView.text = "ET C'EST GAGNÉ !!!!!!!"
         }
     }
 }
