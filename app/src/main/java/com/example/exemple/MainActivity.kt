b0a442cb52dd612a22cf4704a070607846a384a9
Nicolas Quatela <nicolas.quatela@etu?univ-cotedazur.fr>
2025-01-06 11:21:14 +0100

    etape 1

--- app/src/main/java/com/example/exemple/MainActivity.kt
+++ app/src/main/java/com/example/exemple/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.exemple
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,9 +15,24 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val textView: TextView = findViewById(R.id.salut)
-        val button = findViewById<Button>(R.id.button2)
+        val rollButton: Button = findViewById(R.id.button3)
 
-        textView.text = "Hello world"
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView2)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
